6b33c74c8be529d50c8368aad8e6cb2ef0b2e6da
Yomna Abdelfattah <yomnaahmed2211@gmail.com>
2024-10-07 03:48:10 +0300

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#home">FitLife Hub</a>
+        <a class="navbar-brand" href="{{ route('index') }}">FitLife Hub</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
