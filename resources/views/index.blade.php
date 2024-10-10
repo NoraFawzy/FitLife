b5dbe2869b3ca183f720cfc5914b0d66ba005523
Yomna Abdelfattah <yomnaahmed2211@gmail.com>
2024-10-10 20:06:20 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -145,6 +145,7 @@
                         <th>Thu</th>
                         <th>Fri</th>
                         <th>Sat</th>
+                        <th>Sun</th>
                         </thead>
 
                         <tbody>
@@ -155,6 +156,7 @@
                             <td></td>
                             <td></td>
                             <td><strong>Yoga Section</strong><span>7:00 am - 9:00 am</span></td>
+                            
                         </tr>
                         <tr>
                             <td><small>9:00 am</small></td>
@@ -164,6 +166,9 @@
                             <td><strong>Areobic</strong><span>8:00 am - 9:00 am</span></td>
                             <td></td>
                             <td><strong>Cardio</strong><span>8:00 am - 9:00 am</span></td>
+                            <td><strong>Areobic</strong><span>8:00 am - 9:00 am</span></td>
+
+
                         </tr>
                         <tr>
                             <td><small>11:00 am</small></td>
@@ -181,6 +186,8 @@
                             <td><strong>Cardio</strong><span>6:00 pm - 9:00 pm</span></td>
                             <td></td>
                             <td><strong>Crossfit</strong><span>5:00 pm - 7:00 pm</span></td>
+                            <td><strong>Areobic</strong><span>8:00 am - 9:00 am</span></td>
+
                         </tr>
                         </tbody>
                     </table>
