32dfa43d711e13208345ce42433baeba0522cc46
Yomna Abdelfattah <yomnaahmed2211@gmail.com>
2024-10-14 15:19:22 +0300

    class

--- resources/views/admin/classes-list.blade.php
+++ resources/views/admin/classes-list.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classes List</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-</head>
+
 <body class="mt-5 pt-5">
 
     <h1 class="text-center mt-5 display-4" style="color:#E85C0D;">
